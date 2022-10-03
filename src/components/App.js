7b20b7b6569051df1a8739b12f4ea11133afd494
ukrbride4u <ukrbride4u@gmail.com>
2022-10-03 12:32:25 +0300

    starting

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import fetchImages from "../fetch/images-api";
 import Searchbar from "./Searchbar/Searchbar";
 import ImageGallery from "./ImageGallery/ImageGallery";
@@ -6,137 +6,172 @@ import LoadMore from "./LoadMore/LoadMore";
 import Loader from "./Loader/Loader";
 import Modal from "./Modal/Modal";
 
-class App extends Component {
-  state = {
-    imageName: "",
-    page: 1,
-    imagesOnPage: 0,
-    totalImages: 0,
-    isLoading: false,
-    showModal: false,
-    images: null,
-    error: null,
-    currentImageUrl: null,
-    currentImageDescription: null,
-  };
+export default function App() {
+  const [imageName, setImageName] = useState("");
+  const [page, setPage] = useState(1);
+  const [imagesOnPage, setImagesOnPage] = useState(0);
+  const [totalImages, setTotalImages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [images, setImages] = useState(null);
+  const [error, setError] = useState(null);
+  const [currentImageUrl, setCurrentImageUrl] = useState(null);
+  const [currentImageDescription, setCurrentImageDescription] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { imageName, page } = this.state;
+  useEffect(() => {
+    setIsLoading((prevState) => !isLoading);
 
-    if (prevState.imageName !== imageName) {
-      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      fetchImages(imageName)
-        .then(({ hits, totalHits }) => {
-          const imagesArray = hits.map((hit) => ({
-            id: hit.id,
-            description: hit.tags,
-            smallImage: hit.webformatURL,
-            largeImage: hit.largeImageURL,
-          }));
-
-          return this.setState({
-            page: 1,
-            images: imagesArray,
-            imagesOnPage: imagesArray.length,
-            totalImages: totalHits,
-          });
-        })
-        .catch((error) => this.setState({ error }))
-        .finally(() =>
-          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-        );
-    }
-
-    if (prevState.page !== page && page !== 1) {
-      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      fetchImages(imageName, page)
-        .then(({ hits }) => {
-          const imagesArray = hits.map((hit) => ({
-            id: hit.id,
-            description: hit.tags,
-            smallImage: hit.webformatURL,
-            largeImage: hit.largeImageURL,
-          }));
-
-          return this.setState(({ images, imagesOnPage }) => {
-            return {
-              images: [...images, ...imagesArray],
-              imagesOnPage: imagesOnPage + imagesArray.length,
-            };
-          });
-        })
-        .catch((error) => this.setState({ error }))
-        .finally(() =>
-          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-        );
-    }
-  }
-
-  getSearchRequest = (imageName) => {
-    this.setState({ imageName });
-  };
-
-  onNextFetch = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  openModal = (e) => {
-    const currentImageUrl = e.target.dataset.large;
-    const currentImageDescription = e.target.alt;
-
-    if (e.target.nodeName === "IMG") {
-      this.setState(({ showModal }) => ({
-        showModal: !showModal,
-        currentImageUrl: currentImageUrl,
-        currentImageDescription: currentImageDescription,
-      }));
-    }
-  };
-
-  render() {
-    const {
-      images,
-      imagesOnPage,
-      totalImages,
-      isLoading,
-      showModal,
-      currentImageUrl,
-      currentImageDescription,
-    } = this.state;
-
-    const getSearchRequest = this.getSearchRequest;
-    const onNextFetch = this.onNextFetch;
-    const openModal = this.openModal;
-    const toggleModal = this.toggleModal;
-
-    return (
-      <div>
-        <Searchbar onSubmit={getSearchRequest} />
-
-        {images && <ImageGallery images={images} openModal={openModal} />}
-
-        {isLoading && <Loader />}
-
-        {imagesOnPage >= 12 && imagesOnPage < totalImages && (
-          <LoadMore onNextFetch={onNextFetch} />
-        )}
-
-        {showModal && (
-          <Modal
-            onClose={toggleModal}
-            currentImageUrl={currentImageUrl}
-            currentImageDescription={currentImageDescription}
-          />
-        )}
-      </div>
-    );
-  }
+    fetchImages(imageName)
+      .then(({ hits, totalHits }) => {
+        const imagesArray = hits.map((hit) => ({
+          id: hit.id,
+          description: hit.tags,
+          smallImage: hit.webformatURL,
+          largeImage: hit.largeImageURL,
+        }));
+              
+        setImages(imagesArray);
+        setImagesOnPage(imagesArray.length);
+        setTotalImages(totalHits);
+      })
+             .catch((error) => setError(error))
+            .finally(() =>
+              this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
+           );
+  
 }
 
-export default App;
+// class App extends Component {
+//   state = {
+//     imageName: "",
+//     page: 1,
+//     imagesOnPage: 0,
+//     totalImages: 0,
+//     isLoading: false,
+//     showModal: false,
+//     images: null,
+//     error: null,
+//     currentImageUrl: null,
+//     currentImageDescription: null,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { imageName, page } = this.state;
+
+//     if (prevState.imageName !== imageName) {
+//       this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
+
+//       fetchImages(imageName)
+//         .then(({ hits, totalHits }) => {
+//           const imagesArray = hits.map((hit) => ({
+//             id: hit.id,
+//             description: hit.tags,
+//             smallImage: hit.webformatURL,
+//             largeImage: hit.largeImageURL,
+//           }));
+
+//           return this.setState({
+//             page: 1,
+//             images: imagesArray,
+//             imagesOnPage: imagesArray.length,
+//             totalImages: totalHits,
+//           });
+//         })
+//         .catch((error) => this.setState({ error }))
+//         .finally(() =>
+//           this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
+//         );
+//     }
+
+//     if (prevState.page !== page && page !== 1) {
+//       this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
+
+//       fetchImages(imageName, page)
+//         .then(({ hits }) => {
+//           const imagesArray = hits.map((hit) => ({
+//             id: hit.id,
+//             description: hit.tags,
+//             smallImage: hit.webformatURL,
+//             largeImage: hit.largeImageURL,
+//           }));
+
+//           return this.setState(({ images, imagesOnPage }) => {
+//             return {
+//               images: [...images, ...imagesArray],
+//               imagesOnPage: imagesOnPage + imagesArray.length,
+//             };
+//           });
+//         })
+//         .catch((error) => this.setState({ error }))
+//         .finally(() =>
+//           this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
+//         );
+//     }
+//   }
+
+//   getSearchRequest = (imageName) => {
+//     this.setState({ imageName });
+//   };
+
+//   onNextFetch = () => {
+//     this.setState(({ page }) => ({ page: page + 1 }));
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({ showModal: !showModal }));
+//   };
+
+//   openModal = (e) => {
+//     const currentImageUrl = e.target.dataset.large;
+//     const currentImageDescription = e.target.alt;
+
+//     if (e.target.nodeName === "IMG") {
+//       this.setState(({ showModal }) => ({
+//         showModal: !showModal,
+//         currentImageUrl: currentImageUrl,
+//         currentImageDescription: currentImageDescription,
+//       }));
+//     }
+//   };
+
+//   render() {
+//     const {
+//       images,
+//       imagesOnPage,
+//       totalImages,
+//       isLoading,
+//       showModal,
+//       currentImageUrl,
+//       currentImageDescription,
+//     } = this.state;
+
+//     const getSearchRequest = this.getSearchRequest;
+//     const onNextFetch = this.onNextFetch;
+//     const openModal = this.openModal;
+//     const toggleModal = this.toggleModal;
+
+//     return (
+//       <div>
+//         <Searchbar onSubmit={getSearchRequest} />
+
+//         {images && <ImageGallery images={images} openModal={openModal} />}
+
+//         {isLoading && <Loader />}
+
+//         {imagesOnPage >= 12 && imagesOnPage < totalImages && (
+//           <LoadMore onNextFetch={onNextFetch} />
+//         )}
+
+//         {showModal && (
+//           <Modal
+//             onClose={toggleModal}
+//             currentImageUrl={currentImageUrl}
+//             currentImageDescription={currentImageDescription}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,50 +1,46 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import css from "./Searchbar.module.css";
 import { ImSearch } from "react-icons/im";
 
-export default class Searchbar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  state = {
-    imageName: "",
-  };
-  handleNameChange = (e) => {
-    this.setState({
-      imageName: e.currentTarget.value.toLowerCase(),
-    });
+export default function Searchbar({ onSubmit }) {
+  const [imageName, setImageName] = useState("");
+
+  const handleNameChange = (e) => {
+    setImageName(e.currentTarget.value.toLowerCase());
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.imageName.trim() === "") {
+    if (imageName.trim() === "") {
       return alert("Insert some name");
     }
-    this.props.onSubmit(this.state.imageName);
+    onSubmit(imageName);
   };
 
-  render() {
-    return (
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchForm_button}>
-            <ImSearch style={{ marginRight: 8 }}></ImSearch>
-            <span className={css.SearchForm_button_label}>Search</span>
-          </button>
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchForm_button}>
+          <ImSearch style={{ marginRight: 8 }}></ImSearch>
+          <span className={css.SearchForm_button_label}>Search</span>
+        </button>
 
-          <input
-            className={css.SearchForm_input}
-            type="text"
-            name="imageName"
-            value={this.state.imageName}
-            onChange={this.handleNameChange}
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={css.SearchForm_input}
+          type="text"
+          name="imageName"
+          value={imageName}
+          onChange={handleNameChange}
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};

0139ac52b88f98c6433d76f57d7810f331a3f8ac
ukrbride4u <ukrbride4u@gmail.com>
2022-10-04 17:44:54 +0300

    first step

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,10 @@ export default function App() {
   const [currentImageDescription, setCurrentImageDescription] = useState(null);
 
   useEffect(() => {
-    setIsLoading((prevState) => !isLoading);
+    if (!imageName) {
+      return;
+    }
+    setIsLoading((prevState) => !prevState);
 
     fetchImages(imageName)
       .then(({ hits, totalHits }) => {
@@ -29,149 +32,77 @@ export default function App() {
           smallImage: hit.webformatURL,
           largeImage: hit.largeImageURL,
         }));
-              
+
         setImages(imagesArray);
         setImagesOnPage(imagesArray.length);
         setTotalImages(totalHits);
       })
-             .catch((error) => setError(error))
-            .finally(() =>
-              this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-           );
-  
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => setIsLoading((prevState) => !prevState));
+  }, [imageName]);
+
+  useEffect(() => {
+    setIsLoading((prevState) => !prevState);
+
+    fetchImages(imageName, page)
+      .then(({ hits }) => {
+        const imagesArray = hits.map((hit) => ({
+          id: hit.id,
+          description: hit.tags,
+          smallImage: hit.webformatURL,
+          largeImage: hit.largeImageURL,
+        }));
+
+        setImages([...images, ...imagesArray]);
+        setImagesOnPage((prevState) => prevState + imagesArray.length);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading((prevState) => !prevState));
+  }, [page, imageName]);
+
+  const getSearchRequest = (imageName) => {
+    setImageName(imageName);
+  };
+
+  const onNextFetch = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+  const toggleModal = () => {
+    setShowModal((prevState) => !prevState);
+  };
+
+  const openModal = (e) => {
+    const currentImageUrl = e.target.dataset.large;
+    const currentImageDescription = e.target.alt;
+
+    if (e.target.nodeName === "IMG") {
+      setShowModal((prevModal) => !prevModal);
+      setCurrentImageUrl(currentImageUrl);
+      setCurrentImageDescription(currentImageDescription);
+    }
+  };
+
+  return (
+    <div>
+      <Searchbar onSubmit={getSearchRequest} />
+
+      {images && <ImageGallery images={images} openModal={openModal} />}
+
+      {isLoading && <Loader />}
+
+      {imagesOnPage >= 12 && imagesOnPage < totalImages && (
+        <LoadMore onNextFetch={onNextFetch} />
+      )}
+
+      {showModal && (
+        <Modal
+          onClose={toggleModal}
+          currentImageUrl={currentImageUrl}
+          currentImageDescription={currentImageDescription}
+        />
+      )}
+    </div>
+  );
 }
-
-// class App extends Component {
-//   state = {
-//     imageName: "",
-//     page: 1,
-//     imagesOnPage: 0,
-//     totalImages: 0,
-//     isLoading: false,
-//     showModal: false,
-//     images: null,
-//     error: null,
-//     currentImageUrl: null,
-//     currentImageDescription: null,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { imageName, page } = this.state;
-
-//     if (prevState.imageName !== imageName) {
-//       this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-//       fetchImages(imageName)
-//         .then(({ hits, totalHits }) => {
-//           const imagesArray = hits.map((hit) => ({
-//             id: hit.id,
-//             description: hit.tags,
-//             smallImage: hit.webformatURL,
-//             largeImage: hit.largeImageURL,
-//           }));
-
-//           return this.setState({
-//             page: 1,
-//             images: imagesArray,
-//             imagesOnPage: imagesArray.length,
-//             totalImages: totalHits,
-//           });
-//         })
-//         .catch((error) => this.setState({ error }))
-//         .finally(() =>
-//           this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-//         );
-//     }
-
-//     if (prevState.page !== page && page !== 1) {
-//       this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-//       fetchImages(imageName, page)
-//         .then(({ hits }) => {
-//           const imagesArray = hits.map((hit) => ({
-//             id: hit.id,
-//             description: hit.tags,
-//             smallImage: hit.webformatURL,
-//             largeImage: hit.largeImageURL,
-//           }));
-
-//           return this.setState(({ images, imagesOnPage }) => {
-//             return {
-//               images: [...images, ...imagesArray],
-//               imagesOnPage: imagesOnPage + imagesArray.length,
-//             };
-//           });
-//         })
-//         .catch((error) => this.setState({ error }))
-//         .finally(() =>
-//           this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-//         );
-//     }
-//   }
-
-//   getSearchRequest = (imageName) => {
-//     this.setState({ imageName });
-//   };
-
-//   onNextFetch = () => {
-//     this.setState(({ page }) => ({ page: page + 1 }));
-//   };
-
-//   toggleModal = () => {
-//     this.setState(({ showModal }) => ({ showModal: !showModal }));
-//   };
-
-//   openModal = (e) => {
-//     const currentImageUrl = e.target.dataset.large;
-//     const currentImageDescription = e.target.alt;
-
-//     if (e.target.nodeName === "IMG") {
-//       this.setState(({ showModal }) => ({
-//         showModal: !showModal,
-//         currentImageUrl: currentImageUrl,
-//         currentImageDescription: currentImageDescription,
-//       }));
-//     }
-//   };
-
-//   render() {
-//     const {
-//       images,
-//       imagesOnPage,
-//       totalImages,
-//       isLoading,
-//       showModal,
-//       currentImageUrl,
-//       currentImageDescription,
-//     } = this.state;
-
-//     const getSearchRequest = this.getSearchRequest;
-//     const onNextFetch = this.onNextFetch;
-//     const openModal = this.openModal;
-//     const toggleModal = this.toggleModal;
-
-//     return (
-//       <div>
-//         <Searchbar onSubmit={getSearchRequest} />
-
-//         {images && <ImageGallery images={images} openModal={openModal} />}
-
-//         {isLoading && <Loader />}
-
-//         {imagesOnPage >= 12 && imagesOnPage < totalImages && (
-//           <LoadMore onNextFetch={onNextFetch} />
-//         )}
-
-//         {showModal && (
-//           <Modal
-//             onClose={toggleModal}
-//             currentImageUrl={currentImageUrl}
-//             currentImageDescription={currentImageDescription}
-//           />
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,50 +1,93 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 import { createPortal } from "react-dom";
 import css from "./Modal.module.css";
 
 const modalRoot = document.querySelector("#modal-root");
 
-export default class Modal extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    onClose: PropTypes.func.isRequired,
-    currentImageUrl: PropTypes.string,
-    currentImageDescription: PropTypes.string,
-  };
-
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  handleKeyDown = (e) => {
+export default function Modal({
+  onClose,
+  currentImageUrl,
+  currentImageDescription,
+}) {
+  const handleKeyDown = (e) => {
     if (e.code === "Escape") {
-      this.props.onClose();
+      onClose();
     }
   };
-  handleBackdropClick = (e) => {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-
-  render() {
-    const { currentImageUrl, currentImageDescription } = this.props;
-
-    return createPortal(
-      <div className={css.Overlay} onClick={this.handleBackdropClick}>
-        <div className={css.Modal}>
-          <img
-            src={currentImageUrl}
-            alt={currentImageDescription}
-            loading="lazy"
-          />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={css.Overlay} onClick={handleBackdropClick}>
+      <div className={css.Modal}>
+        <img
+          src={currentImageUrl}
+          alt={currentImageDescription}
+          loading="lazy"
+        />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
+
+Modal.propTypes = {
+  title: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+  currentImageUrl: PropTypes.string,
+  currentImageDescription: PropTypes.string,
+};
+
+// export default class Modal extends Component {
+//   static propTypes = {
+//     title: PropTypes.string,
+//     onClose: PropTypes.func.isRequired,
+//     currentImageUrl: PropTypes.string,
+//     currentImageDescription: PropTypes.string,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.handleKeyDown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener("keydown", this.handleKeyDown);
+//   }
+
+//   handleKeyDown = (e) => {
+//     if (e.code === "Escape") {
+//       this.props.onClose();
+//     }
+//   };
+//   handleBackdropClick = (e) => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { currentImageUrl, currentImageDescription } = this.props;
+
+//     return createPortal(
+//       <div className={css.Overlay} onClick={this.handleBackdropClick}>
+//         <div className={css.Modal}>
+//           <img
+//             src={currentImageUrl}
+//             alt={currentImageDescription}
+//             loading="lazy"
+//           />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
